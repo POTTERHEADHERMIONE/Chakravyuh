5d4f0ec64e1708ea064830a344122382be68f696
Pranjal Singh <97018332+PranjalSingh2608@users.noreply.github.com>
2023-11-14 14:00:35 +0530

    1.5

--- chakravyuh/settings.py
+++ chakravyuh/settings.py
@@ -16,6 +16,7 @@ import environ
 # Initialise environment variables
 env = environ.Env()
 environ.Env.read_env()
+SECRET_KEY = env('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
